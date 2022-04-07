e970b4382b9a80260e4c1d18571c9519198de77f
Maximko <1vanskiyyyyy@gmail.com>
2022-04-07 14:02:24 +0300

    corect brands

--- js/home.js
+++ js/home.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $('.header-burger').click(function (event) {
         $('.header-burger, .header-menu').toggleClass('active');
+        //for header-burger menu
     });
     $('.header-burger').click(function (event) {
         $('body').toggleClass('lock');
+        //for lock scroll
     });
 });
 
